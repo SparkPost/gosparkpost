32eee8385d57aed098eae7e3817f0dadea1f09ff
Dave Gray <github@doesntsuck.com>
2015-09-11 14:15:49 -0600

    return on error to stop test case; call Create method; rework how we embed the helper object into the per-api object; add constructor; only Marshal Options if there were some specified; allow map[string]string since that's how it makes sense to write it out by hand; add validation function; finish up Templates.CreateWithTemplate

--- api/templates/template_test.go
+++ api/templates/template_test.go
@@ -1,6 +1,7 @@
 package templates
 
 import (
+	"fmt"
 	"testing"
 
 	"bitbucket.org/yargevad/go-sparkpost/config"
@@ -11,11 +12,29 @@ func TestTemplates(t *testing.T) {
 	cfg, err := config.Load("../../config.json")
 	if err != nil {
 		t.Error(err)
+		return
 	}
 
-	var tAPI Templates
-	err = tAPI.Init(cfg)
+	T, err = New(cfg)
 	if err != nil {
 		t.Error(err)
+		return
 	}
+
+	content := Content{
+		Subject: "this is a test template",
+		//Text:    "text part of the test template {{a}",
+		Text: "text part of the test template",
+		From: map[string]string{
+			"name":  "test name",
+			"email": "test@email.com",
+		},
+	}
+
+	id, err := T.Create("test template", content)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	fmt.Printf("Created Template with id=%s\n", id)
 }
--- api/templates/templates.go
+++ api/templates/templates.go
@@ -1,26 +1,39 @@
 package templates
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"reflect"
 	"strings"
 
 	"bitbucket.org/yargevad/go-sparkpost/api"
+	"bitbucket.org/yargevad/go-sparkpost/config"
 )
 
 type Templates struct {
-	*api.API
-	Path   string
-	Errors []api.Error
+	api.API
+
+	Path     string
+	Response *api.Response
+}
+
+func New(cfg *config.Config) (*Templates, error) {
+	t := &Templates{}
+	err := t.Init(cfg)
+	if err != nil {
+		return nil, err
+	}
+	return t
 }
 
 type Template struct {
-	ID          string  `json:"id,omitempty"`
-	Content     Content `json:"content,omitempty"`
-	Published   bool    `json:"published,omitempty"`
-	Name        string  `json:"name,omitempty"`
-	Description string  `json:"description,omitempty"`
-	Options     Options `json:"options,omitempty"`
+	ID          string   `json:"id,omitempty"`
+	Content     Content  `json:"content,omitempty"`
+	Published   bool     `json:"published,omitempty"`
+	Name        string   `json:"name,omitempty"`
+	Description string   `json:"description,omitempty"`
+	Options     *Options `json:"options,omitempty"`
 }
 
 type Content struct {
@@ -44,6 +57,11 @@ type Options struct {
 	Transactional bool `json:"transactional,omitempty"`
 }
 
+func (t *Templates) Init(cfg *config.Config) (err error) {
+	t.Path = fmt.Sprintf("/api/v%d/templates", cfg.ApiVersion)
+	return t.API.Init(cfg)
+}
+
 func ParseFrom(from interface{}) (f From, err error) {
 	// handle both of the allowed types
 	switch fromVal := from.(type) {
@@ -54,7 +72,8 @@ func ParseFrom(from interface{}) (f From, err error) {
 			f.Email = fromVal
 		}
 
-	case map[string]interface{}: // nested json object
+	case map[string]interface{}:
+		// auto-parsed nested json object
 		for k, v := range fromVal {
 			switch vVal := v.(type) {
 			case string:
@@ -69,6 +88,16 @@ func ParseFrom(from interface{}) (f From, err error) {
 			}
 		}
 
+	case map[string]string:
+		// user-provided json literal (convenience)
+		for k, v := range fromVal {
+			if strings.EqualFold(k, "name") {
+				f.Name = v
+			} else if strings.EqualFold(k, "email") {
+				f.Email = v
+			}
+		}
+
 	default:
 		err = fmt.Errorf("unsupported Content.From value type [%s]", reflect.TypeOf(fromVal))
 	}
@@ -76,40 +105,121 @@ func ParseFrom(from interface{}) (f From, err error) {
 	return
 }
 
+func (t Template) Validate() error {
+	if t.Content.EmailRFC822 != "" {
+		// TODO: optionally validate MIME structure
+		// if MIME content is present, clobber all other Content options
+		t.Content = Content{EmailRFC822: t.Content.EmailRFC822}
+		return nil
+	}
+
+	// enforce required parameters
+	if t.Name == "" && t.ID == "" {
+		return fmt.Errorf("Template requires a name or id")
+	} else if t.Content.Subject == "" {
+		return fmt.Errorf("Template requires a non-empty Content.Subject")
+	} else if t.Content.HTML == "" && t.Content.Text == "" {
+		return fmt.Errorf("Template requires either Content.HTML or Content.Text")
+	}
+	_, err := ParseFrom(t.Content.From)
+	if err != nil {
+		return err
+	}
+
+	// enforce max lengths
+	// TODO: enforce 15MB Content limit
+	if len(t.ID) > 64 {
+		return fmt.Errorf("Template id may not be longer than 64 bytes")
+	} else if len(t.Name) > 1024 {
+		return fmt.Errorf("Template name may not be longer than 1024 bytes")
+	} else if len(t.Description) > 1024 {
+		return fmt.Errorf("Template description may not be longer than 1024 bytes")
+	}
+
+	return nil
+}
+
 // Helper function for the "at a minimum" case mentioned in the SparkPost API docs:
 // https://www.sparkpost.com/api#/reference/templates/create-and-list/create-a-template
 func (t Templates) Create(name string, content Content) (id string, err error) {
-	if name == "" {
-		err = fmt.Errorf("templates.Create requires a name")
-		return
-	} else if content.Subject == "" {
-		err = fmt.Errorf("templates.Create requires a non-empty Content.Subject")
-		return
-	} else if content.HTML == "" && content.Text == "" {
-		err = fmt.Errorf("templates.Create requires either Content.HTML or Content.Text")
-		return
+	template := &Template{
+		Name:    name,
+		Content: content,
 	}
-	_, err = ParseFrom(content.From)
+	err = template.Validate()
 	if err != nil {
 		return
 	}
 
-	template := Template{
-		Name:    name,
-		Content: content,
-	}
 	id, err = t.CreateWithTemplate(template)
-
 	return
 }
 
 // Support for all Template API options.
 // Helper functions call into this function after building a Template object.
 // Validates input before making request.
-func (t Templates) CreateWithTemplate(template Template) (id string, err error) {
+func (t Templates) CreateWithTemplate(template *Template) (id string, err error) {
+	if template == nil {
+		err = fmt.Errorf("CreateWithTemplate called with nil Template")
+		return
+	}
+
+	err = template.Validate()
+	if err != nil {
+		return
+	}
+
+	jsonBytes, err := json.Marshal(template)
+	if err != nil {
+		return
+	}
+
+	url := fmt.Sprintf("%s%s", t.API.Config.ApiBase, t.Path)
+	res, err := t.Post(url, jsonBytes)
+	if err != nil {
+		return
+	}
+
+	if err = api.AssertJson(res); err != nil {
+		return
+	}
+
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return
+	}
+
+	apiResponse := &api.Response{}
+	apiResponse.HTTP = res
+	// pull out either "results" or "errors"
+	err = json.Unmarshal(body, apiResponse)
+	if err != nil {
+		return
+	}
+	// keep this around so callers can see failure details
+	t.Response = apiResponse
+
+	if len(apiResponse.Errors) > 0 {
+		err = fmt.Errorf("Error creating Template:\n%s", string(body))
+	} else if len(apiResponse.Results) > 0 {
+		var ok bool
+		id, ok = apiResponse.Results["id"]
+		if !ok {
+			err = fmt.Errorf("Unexpected response to Template creation:\n%s", string(body))
+		}
+	}
+
 	return
 }
 
 func (t Templates) CreateWithJSON(j string) (id string, err error) {
+	template := &Template{}
+	err = json.Unmarshal([]byte(j), template)
+	if err != nil {
+		return
+	}
+
+	id, err = t.CreateWithTemplate(template)
 	return
 }
