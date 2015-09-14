7995d81c09aeeca7ed95732b9541cce7949dd4c7
Dave Gray <github@doesntsuck.com>
2015-09-14 16:33:05 -0600

    doc coverage for api helper package

--- api/api.go
+++ api/api.go
@@ -1,3 +1,4 @@
+// Package api provides structures and functions used by other SparkPost API packages.
 package api
 
 import (
@@ -12,16 +13,20 @@ import (
 	certifi "github.com/certifi/gocertifi"
 )
 
+// Config includes all information necessary to make an API request.
 type Config struct {
 	BaseUrl string
 	ApiKey  string
 }
 
+// API exists to be embedded in other API objects.
 type API struct {
 	Config *Config
 	Client *http.Client
 }
 
+// Response contains information about the last HTTP response.
+// Helpful when an error message doesn't necessarily give the complete picture.
 type Response struct {
 	HTTP    *http.Response
 	Body    string
@@ -29,6 +34,7 @@ type Response struct {
 	Errors  []Error           `json:"errors,omitempty"`
 }
 
+// The error structure returned by SparkPost APIs.
 type Error struct {
 	Message     string `json:"message"`
 	Code        string `json:"code"`
@@ -37,6 +43,7 @@ type Error struct {
 	Line        int    `json:"line,omitempty"`
 }
 
+// Init pulls together everything necessary to make an API request.
 func (api *API) Init(cfg *Config) (err error) {
 	api.Config = cfg
 
@@ -57,7 +64,8 @@ func (api *API) Init(cfg *Config) (err error) {
 	return
 }
 
-// Send a Post request with the provided JSON payload to the specified url.
+// HttpPost sends a Post request with the provided JSON payload to the specified url.
+// Query params are supported via net/url - roll your own and stringify it.
 // Authenticate using the configured API key.
 func (api *API) HttpPost(url string, data []byte) (*http.Response, error) {
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
@@ -69,8 +77,8 @@ func (api *API) HttpPost(url string, data []byte) (*http.Response, error) {
 	return api.Client.Do(req)
 }
 
-// Send a Get request to the specified url.
-// Query params are supported via net/url - pass in url.String()
+// HttpGet sends a Get request to the specified url.
+// Query params are supported via net/url - roll your own and stringify it.
 // Authenticate using the configured API key.
 func (api *API) HttpGet(url string) (*http.Response, error) {
 	req, err := http.NewRequest("GET", url, nil)
@@ -81,7 +89,8 @@ func (api *API) HttpGet(url string) (*http.Response, error) {
 	return api.Client.Do(req)
 }
 
-// Send a Delete request to the provided url.
+// HttpDelete sends a Delete request to the provided url.
+// Query params are supported via net/url - roll your own and stringify it.
 // Authenticate using the configured API key.
 func (api *API) HttpDelete(url string) (*http.Response, error) {
 	req, err := http.NewRequest("DELETE", url, nil)
@@ -92,11 +101,14 @@ func (api *API) HttpDelete(url string) (*http.Response, error) {
 	return api.Client.Do(req)
 }
 
+// ReadBody is a convenience wrapper for the response body.
 func ReadBody(res *http.Response) ([]byte, error) {
 	defer res.Body.Close()
 	return ioutil.ReadAll(res.Body)
 }
 
+// ParseApiResponse pulls info from JSON http responses into api.Response object.
+// It's helpful to call api.AssertJson before calling this function.
 func ParseApiResponse(res *http.Response) (*Response, error) {
 	body, err := ReadBody(res)
 	if err != nil {
@@ -114,7 +126,7 @@ func ParseApiResponse(res *http.Response) (*Response, error) {
 	return apiRes, nil
 }
 
-// Return an error if the provided HTTP response isn't JSON.
+// AssertJson returns an error if the provided HTTP response isn't JSON.
 func AssertJson(res *http.Response) error {
 	if res == nil {
 		return fmt.Errorf("AssertJson got nil http.Response")
@@ -126,12 +138,14 @@ func AssertJson(res *http.Response) error {
 	return nil
 }
 
+// PrettyError returns a human-readable error message for common http errors returned by the API.
 func PrettyError(noun, verb string, res *http.Response) error {
 	if res.StatusCode == 404 {
 		return fmt.Errorf("%s does not exist, %s failed.", noun, verb)
 	} else if res.StatusCode == 401 {
 		return fmt.Errorf("%s %s failed, permission denied. Check your API key.", noun, verb)
 	} else if res.StatusCode == 403 {
+		// This is what happens if an endpoint URL gets typo'd. (dgray 2015-09-14)
 		return fmt.Errorf("%s %s failed. Are you using the right API path?", noun, verb)
 	}
 	return nil
