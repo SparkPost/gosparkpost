c98bbeb24b37773d5728f2125599da6e18fcbcf1
Dave Gray <bitbucket@doesntsuck.com>
2015-10-26 10:53:44 -0600

    fix compile for template json parsing example

--- api/templates/from_json_test.go
+++ api/templates/from_json_test.go
@@ -2,21 +2,13 @@ package templates_test
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 
-	"github.com/SparkPost/go-sparkpost/api"
 	"github.com/SparkPost/go-sparkpost/api/templates"
 )
 
 // Build a native Go Template structure from a JSON string
 func ExampleTemplate() {
-	cfg := api.Config{BaseUrl: "https://example.com", ApiKey: "foo"}
-	T, err := templates.New(cfg)
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	template := &templates.Template{}
 	jsonStr := `{
 		"name": "testy template",
@@ -30,7 +22,7 @@ func ExampleTemplate() {
 			"reply_to": "tester@example.com"
 		}
 	}`
-	err = json.Unmarshal([]byte(jsonStr), template)
+	err := json.Unmarshal([]byte(jsonStr), template)
 	if err != nil {
 		log.Fatal(err)
 	}
