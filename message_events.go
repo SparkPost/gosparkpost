302534314e47e3dc5cc50147c2e222cc5cdb11aa
Chris Wilson <github@yepher.com>
2016-02-11 15:57:48 -0600

    Proposed Events parsing changes

--- message_events.go
+++ message_events.go
@@ -4,33 +4,80 @@ import (
 	"encoding/json"
 	"fmt"
 	URL "net/url"
-	"os"
-	re "regexp"
 	"strings"
-
-	"github.com/SparkPost/gosparkpost/events"
 )
 
 // https://www.sparkpost.com/api#/reference/message-events
 var messageEventsPathFormat = "/api/v%d/message-events"
 
+type EventsWrapper struct {
+	Results []struct {
+		Type string `json:"type"`
+
+		CustomerID      string            `json:"customer_id"`
+		DelvMethod      string            `json:"delv_method,omitempty"`
+		EventID         string            `json:"event_id"`
+		FriendlyFrom    string            `json:"friendly_from,omitempty"`
+		IPAddress       string            `json:"ip_address,omitempty"`
+		MessageID       string            `json:"message_id,omitempty"`
+		MsgFrom         string            `json:"msg_from"`
+		MsgSize         string            `json:"msg_size,omitempty"`
+		NumRetries      string            `json:"num_retries,omitempty"`
+		QueueTime       string            `json:"queue_time,omitempty"`
+		RawRcptTo       string            `json:"raw_rcpt_to"`
+		RcptMeta        map[string]string `json:"rcpt_meta,omitempty"`
+		RcptTags        []interface{}     `json:"rcpt_tags,omitempty"`
+		RcptTo          string            `json:"rcpt_to"`
+		RoutingDomain   string            `json:"routing_domain,omitempty"`
+		Subject         string            `json:"subject,omitempty"`
+		Tdate           string            `json:"tdate,omitempty"`
+		TemplateID      string            `json:"template_id,omitempty"`
+		TemplateVersion string            `json:"template_version,omitempty"`
+		TransmissionID  string            `json:"transmission_id,omitempty"`
+		Timestamp       string            `json:"timestamp"`
+		ErrorCode       string            `json:"error_code,omitempty"`
+		RawReason       string            `json:"raw_reason,omitempty"`
+		Reason          string            `json:"reason,omitempty"`
+		RemoteAddr      string            `json:"remote_addr,omitempty"`
+	} `json:"results,omitempty"`
+	TotalCount int      `json:"total_count,omitempty"`
+	Links      []string `json:"links,omitempty"`
+}
+
+// https://developers.sparkpost.com/api/#/reference/message-events/events-samples/search-for-message-events
+func (c *Client) SearchMessageEvents(parameters map[string]string) (*EventsWrapper, error) {
+
+	var finalUrl string
+	path := fmt.Sprintf(messageEventsPathFormat, c.Config.ApiVersion)
+	if parameters == nil || len(parameters) == 0 {
+		finalUrl = fmt.Sprintf("%s%s", c.Config.BaseUrl, path)
+	} else {
+		params := URL.Values{}
+		for k, v := range parameters {
+			params.Add(k, v)
+		}
+
+		finalUrl = fmt.Sprintf("%s%s?%s", c.Config.BaseUrl, path, params.Encode())
+	}
+
+	fmt.Println("URL: ", finalUrl)
+
+	return DoRequest(c, finalUrl)
+}
+
 // Samples requests a list of example event data.
-func (c *Client) EventSamples(types *[]string) (*[]events.Event, error) {
+func (c *Client) EventSamples(types *[]string) (*EventsWrapper, error) {
 	// append any requested event types to path
-	var url string
+	var finalUrl string
 	path := fmt.Sprintf(messageEventsPathFormat, c.Config.ApiVersion)
 	if types == nil {
-		url = fmt.Sprintf("%s%s/events/samples", c.Config.BaseUrl, path)
+		finalUrl = fmt.Sprintf("%s%s/events/samples", c.Config.BaseUrl, path)
 	} else {
-		// validate types
-		for _, etype := range *types {
-			if !events.ValidEventType(etype) {
-				return nil, fmt.Errorf("Invalid event type [%s]", etype)
-			}
-		}
+
 		// break up the url into a net.URL object
 		u, err := URL.Parse(fmt.Sprintf("%s%s/events/samples", c.Config.BaseUrl, path))
 		if err != nil {
+			fmt.Println("Error: ", err)
 			return nil, err
 		}
 
@@ -39,11 +86,15 @@ func (c *Client) EventSamples(types *[]string) (*[]events.Event, error) {
 		// add the requested events and re-encode
 		q.Set("events", strings.Join(*types, ","))
 		u.RawQuery = q.Encode()
-		url = u.String()
+		finalUrl = u.String()
 	}
 
+	return DoRequest(c, finalUrl)
+}
+
+func DoRequest(c *Client, finalUrl string) (*EventsWrapper, error) {
 	// Send off our request
-	res, err := c.HttpGet(url)
+	res, err := c.HttpGet(finalUrl)
 	if err != nil {
 		return nil, err
 	}
@@ -66,56 +117,18 @@ func (c *Client) EventSamples(types *[]string) (*[]events.Event, error) {
 	}
 	*/
 
+	myVal := string(bodyBytes)
+
+	fmt.Println(myVal)
+
 	// Parse expected response structure
-	resMap := map[string][]*json.RawMessage{}
+	var resMap EventsWrapper //map[string]interface{}
 	err = json.Unmarshal(bodyBytes, &resMap)
 	if err != nil {
 		// FIXME: better error message
+		fmt.Println("Failed to unmarshal content")
 		return nil, err
 	}
 
-	// If the key "results" isn't present, something bad happened.
-	results, ok := resMap["results"]
-	if !ok {
-		// FIXME: better error message
-		return nil, fmt.Errorf("no results!")
-	}
-
-	return ParseEvents(results)
-}
-
-var typeMatch *re.Regexp = re.MustCompile("\"type\":\\s*\"(\\w+)\"")
-
-func ParseEvents(jlist []*json.RawMessage) (*[]events.Event, error) {
-	eventCount := len(jlist)
-	elist := make([]events.Event, eventCount)
-
-	i := 0
-	for _, j := range jlist {
-		// Coax the type out of the stringified event (sigh)
-		tstr := typeMatch.FindStringSubmatch(string(*j))
-		if len(tstr) < 2 {
-			return nil, fmt.Errorf("ParseEvents didn't find an event type in:\n", string(*j))
-		}
-
-		e := events.EventForName(tstr[1])
-		if e == nil {
-			// TODO: log the offending event and continue
-			fmt.Fprintf(os.Stderr, "unhandled event type [%s]\n", tstr[1])
-			continue
-		}
-
-		// Parse event JSON into native object
-		err := json.Unmarshal([]byte(*j), e)
-		if err != nil {
-			return nil, fmt.Errorf("error parsing [%s]: %s", tstr[1], err)
-		}
-
-		// Link object into the list/array we'll return
-		elist[i] = e
-		i++
-	}
-
-	rv := elist[:i]
-	return &rv, nil
+	return &resMap, err
 }
