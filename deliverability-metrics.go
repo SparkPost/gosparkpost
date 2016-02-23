2c307462b9d6c3780554c82b3c34046927664839
Chris Wilson <github@yepher.com>
2016-02-23 09:14:09 -0600

    Fixes problem where deliverability metrics were not getting decoded because field were private.

--- deliverability-metrics.go
+++ deliverability-metrics.go
@@ -12,44 +12,44 @@ import (
 var deliverabilityMetricPathFormat = "/api/v%d/metrics/deliverability"
 
 type DeliverabilityMetricItem struct {
-	count_injected                 int    `json:"count_injected,omitempty"`
-	count_bounce                   int    `json:"count_bounce,omitempty"`
-	count_rejected                 int    `json:"count_rejected,omitempty"`
-	count_delivered                int    `json:"count_delivered,omitempty"`
-	count_delivered_first          int    `json:"count_delivered_first,omitempty"`
-	count_delivered_subsequent     int    `json:"count_delivered_subsequent,omitempty"`
-	total_delivery_time_first      int    `json:"total_delivery_time_first,omitempty"`
-	total_delivery_time_subsequent int    `json:"total_delivery_time_subsequent,omitempty"`
-	total_msg_volume               int    `json:"total_msg_volume,omitempty"`
-	count_policy_rejection         int    `json:"count_policy_rejection,omitempty"`
-	count_generation_rejection     int    `json:"count_generation_rejection,omitempty"`
-	count_generation_failed        int    `json:"count_generation_failed,omitempty"`
-	count_inband_bounce            int    `json:"count_inband_bounce,omitempty"`
-	count_outofband_bounce         int    `json:"count_outofband_bounce,omitempty"`
-	count_soft_bounce              int    `json:"count_soft_bounce,omitempty"`
-	count_hard_bounce              int    `json:"count_hard_bounce,omitempty"`
-	count_block_bounce             int    `json:"count_block_bounce,omitempty"`
-	count_admin_bounce             int    `json:"count_admin_bounce,omitempty"`
-	count_undetermined_bounce      int    `json:"count_undetermined_bounce,omitempty"`
-	count_delayed                  int    `json:"count_delayed,omitempty"`
-	count_delayed_first            int    `json:"count_delayed_first,omitempty"`
-	count_rendered                 int    `json:"count_rendered,omitempty"`
-	count_unique_rendered          int    `json:"count_unique_rendered,omitempty"`
-	count_unique_confirmed_opened  int    `json:"count_unique_confirmed_opened,omitempty"`
-	count_clicked                  int    `json:"count_clicked,omitempty"`
-	count_unique_clicked           int    `json:"count_unique_clicked,omitempty"`
-	count_targeted                 int    `json:"count_targeted,omitempty"`
-	count_sent                     int    `json:"count_sent,omitempty"`
-	count_accepted                 int    `json:"count_accepted,omitempty"`
-	count_spam_complaint           int    `json:"count_spam_complaint,omitempty"`
-	domain                         string `json:"domain,omitempty"`
+	Count_injected                 int    `json:"count_injected"`
+	Count_bounce                   int    `json:"count_bounce,omitempty"`
+	Count_rejected                 int    `json:"count_rejected,omitempty"`
+	Count_delivered                int    `json:"count_delivered,omitempty"`
+	Count_delivered_first          int    `json:"count_delivered_first,omitempty"`
+	Count_delivered_subsequent     int    `json:"count_delivered_subsequent,omitempty"`
+	Total_delivery_time_first      int    `json:"total_delivery_time_first,omitempty"`
+	Total_delivery_time_subsequent int    `json:"total_delivery_time_subsequent,omitempty"`
+	Total_msg_volume               int    `json:"total_msg_volume,omitempty"`
+	Count_policy_rejection         int    `json:"count_policy_rejection,omitempty"`
+	Count_generation_rejection     int    `json:"count_generation_rejection,omitempty"`
+	Count_generation_failed        int    `json:"count_generation_failed,omitempty"`
+	Count_inband_bounce            int    `json:"count_inband_bounce,omitempty"`
+	Count_outofband_bounce         int    `json:"count_outofband_bounce,omitempty"`
+	Count_soft_bounce              int    `json:"count_soft_bounce,omitempty"`
+	Count_hard_bounce              int    `json:"count_hard_bounce,omitempty"`
+	Count_block_bounce             int    `json:"count_block_bounce,omitempty"`
+	Count_admin_bounce             int    `json:"count_admin_bounce,omitempty"`
+	Count_undetermined_bounce      int    `json:"count_undetermined_bounce,omitempty"`
+	Count_delayed                  int    `json:"count_delayed,omitempty"`
+	Count_delayed_first            int    `json:"count_delayed_first,omitempty"`
+	Count_rendered                 int    `json:"count_rendered,omitempty"`
+	Count_unique_rendered          int    `json:"count_unique_rendered,omitempty"`
+	Count_unique_confirmed_opened  int    `json:"count_unique_confirmed_opened,omitempty"`
+	Count_clicked                  int    `json:"count_clicked,omitempty"`
+	Count_unique_clicked           int    `json:"count_unique_clicked,omitempty"`
+	Count_targeted                 int    `json:"count_targeted,omitempty"`
+	Count_sent                     int    `json:"count_sent,omitempty"`
+	Count_accepted                 int    `json:"count_accepted,omitempty"`
+	Count_spam_complaint           int    `json:"count_spam_complaint,omitempty"`
+	Domain                         string `json:"domain,omitempty"`
 }
 
 type DeliverabilityMetricEventsWrapper struct {
-	Results    []*DeliverabilityMetricItem `json:"results,omitempty"`
+	Results []*DeliverabilityMetricItem `json:"results,omitempty"`
 	TotalCount int                         `json:"total_count,omitempty"`
 	Links      []map[string]string         `json:"links,omitempty"`
-	Errors     []interface{}               `json:"errors,omitempty"`
+	Errors []interface{} `json:"errors,omitempty"`
 	//{"errors":[{"param":"from","message":"From must be before to","value":"2014-07-20T09:00"},{"param":"to","message":"To must be in the format YYYY-MM-DDTHH:mm","value":"now"}]}
 }
 
@@ -81,9 +81,7 @@ func (c *Client) QueryDeliverabilityMetrics(extraPath string, parameters map[str
 
 func (c *Client) MetricEventAsString(e *DeliverabilityMetricItem) string {
 
-	
-
-	return fmt.Sprintf("domain: %s, count_injected: %d", e.domain, e.count_injected)
+	return fmt.Sprintf("domain: %s, [%v]", e.Domain, e)
 }
 
 func DoMetricsRequest(c *Client, finalUrl string) (*DeliverabilityMetricEventsWrapper, error) {
@@ -112,13 +110,12 @@ func DoMetricsRequest(c *Client, finalUrl string) (*DeliverabilityMetricEventsWr
 	*/
 
 	// Parse expected response structure
-	var resMap DeliverabilityMetricEventsWrapper //map[string]interface{}
+	var resMap DeliverabilityMetricEventsWrapper
 	err = json.Unmarshal(bodyBytes, &resMap)
-	log.Printf("Decoded : %s", resMap)
+	
 	if err != nil {
 		return nil, err
 	}
 
 	return &resMap, err
 }
-
