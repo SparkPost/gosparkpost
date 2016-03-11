ba2c87cd5cdea33d975a10631ba64ecfcec24652
Vojtech Vitek (V-Teq) <vojtech.vitek@pressly.com>
2016-03-11 15:10:48 -0500

    Clean-up, remove obsolete code

--- events/events.go
+++ events/events.go
@@ -3,107 +3,22 @@ package events
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
-	"log"
 	"strconv"
 	"time"
 )
 
-// eventTypes contains all of the valid event types
-var eventTypes = map[string]bool{
-	"bounce":               true,
-	"click":                true,
-	"creation":             false,
-	"delay":                true,
-	"delivery":             true,
-	"generation_failure":   true,
-	"generation_rejection": true,
-	"injection":            true,
-	"list_unsubscribe":     true,
-	"link_unsubscribe":     true,
-	"open":                 true,
-	"out_of_band":          true,
-	"policy_rejection":     true,
-	"spam_complaint":       true,
-	"relay_delivery":       true,
-	"relay_injection":      true,
-	"relay_message":        true,
-	"relay_permfail":       true,
-	"relay_rejection":      true,
-	"relay_tempfail":       true,
-}
-
-// ValidEventType returns true if the event name parameter is valid.
-func ValidEventType(eventType string) bool {
-	if _, ok := eventTypes[eventType]; ok {
-		return true
-	}
-	return false
-}
-
-// EventForName returns a struct matching the passed-in type.
-func EventForName(eventType string) Event {
-	if !ValidEventType(eventType) {
-		return nil
-	}
-
-	switch eventType {
-	case "bounce":
-		return &Bounce{}
-	case "click":
-		return &Click{}
-	case "creation":
-		return &Creation{}
-	case "delay":
-		return &Delay{}
-	case "delivery":
-		return &Delivery{}
-	case "generation_failure":
-		return &GenerationFailure{}
-	case "generation_rejection":
-		return &GenerationRejection{}
-	case "injection":
-		return &Injection{}
-	case "list_unsubscribe":
-		return &ListUnsubscribe{}
-	case "link_unsubscribe":
-		return &LinkUnsubscribe{}
-	case "open":
-		return &Open{}
-	case "out_of_band":
-		return &OutOfBand{}
-	case "policy_rejection":
-		return &PolicyRejection{}
-	case "spam_complaint":
-		return &SpamComplaint{}
-	case "relay_delivery":
-		return &RelayDelivery{}
-	case "relay_injection":
-		return &RelayInjection{}
-	case "relay_message":
-		return &RelayMessage{}
-	case "relay_permfail":
-		return &RelayPermfail{}
-	case "relay_rejection":
-		return &RelayRejection{}
-	case "relay_tempfail":
-		return &RelayTempfail{}
-
-	default:
-		return nil
-	}
-}
-
-// Event allows 2+ types of event in a data structure.
+// Event is a generic event.
 type Event interface {
 	EventType() string
 }
 
-// Events is a list of events. Useful for decoding.
+// Events is a list of generic events. Useful for decoding events from API webhooks.
 type Events []Event
 
-func (events Events) UnmarshalJSON(data []byte) error {
-	events = []Event{}
+func (events *Events) UnmarshalJSON(data []byte) error {
+	*events = []Event{}
 
 	var eventWrappers []struct {
 		MsysEventWrapper map[string]json.RawMessage `json:"msys"`
@@ -112,14 +27,12 @@ func (events Events) UnmarshalJSON(data []byte) error {
 		return err
 	}
 
-	for i, wrapper := range eventWrappers {
+	for _, wrapper := range eventWrappers {
 		for _, eventData := range wrapper.MsysEventWrapper {
 			var typeLookup EventCommon
 			if err := json.Unmarshal(eventData, &typeLookup); err != nil {
-				log.Printf("lookup failed: %v %v", eventData)
 				return err
 			}
-			log.Printf("lookup: %v %v", typeLookup.EventType(), typeLookup)
 
 			var event Event
 			switch typeLookup.EventType() {
@@ -164,14 +77,22 @@ func (events Events) UnmarshalJSON(data []byte) error {
 			case "relay_tempfail":
 				event = &RelayTempfail{}
 			default:
-				event = &Unknown{RawJSON: eventData}
+				event = &Unknown{
+					EventCommon: EventCommon{Type: typeLookup.EventType()},
+					RawJSON:     eventData,
+					Error:       errors.New("not implemented"),
+				}
+				*events = append(*events, event)
+				continue
 			}
 			if err := json.Unmarshal(eventData, &event); err != nil {
-				event = &Unknown{RawJSON: eventData, Error: err}
-				log.Printf("cannot parse into %T: %v: %s", event, err, eventData)
+				event = &Unknown{
+					EventCommon: EventCommon{Type: typeLookup.EventType()},
+					RawJSON:     eventData,
+					Error:       err,
+				}
 			}
-			log.Printf("item[%v]: %T %v", i, event, event.EventType())
-			events = append(events, event)
+			*events = append(*events, event)
 		}
 	}
 
@@ -212,6 +133,7 @@ type EventCommon struct {
 func (e EventCommon) EventType() string { return e.Type }
 
 type Unknown struct {
+	EventCommon
 	RawJSON json.RawMessage
 	Error   error
 }
@@ -219,7 +141,7 @@ type Unknown struct {
 func (e *Unknown) EventType() string { return "unknown" }
 
 func (e *Unknown) String() string {
-	return fmt.Sprintf("Unknown event: %v %v", e.Error, e.RawJSON)
+	return fmt.Sprintf("Unknown event (type %q): %v\n%s", e.EventCommon.EventType(), e.Error, e.RawJSON)
 }
 
 func (e *Unknown) UnmarshalJSON(data []byte) error {
--- events/events_test.go
+++ events/events_test.go
@@ -5,12 +5,10 @@ import (
 	"io/ioutil"
 	"os"
 	"testing"
-
-	"github.com/goware/lg"
 )
 
-func TestEvents(t *testing.T) {
-	file, err := os.Open("events.json")
+func TestSampleEvents(t *testing.T) {
+	file, err := os.Open("sample-events.json")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -27,27 +25,8 @@ func TestEvents(t *testing.T) {
 	}
 
 	for _, event := range events {
-		switch e := event.(type) {
-		case *Bounce:
-			lg.Debugf("%v to %v was bounced at %v because of %v (%v)", e.TransmissionID, e.Recipient, e.Timestamp, e.Reason, e.RawReason)
-		case *Delivery:
-			lg.Debugf("%v to %v was delivered at %v", e.TransmissionID, e.Recipient, e.Timestamp)
-		case *Injection:
-			lg.Debugf("%v to %v was injected at %v", e.TransmissionID, e.Recipient, e.Timestamp)
-		case *SpamComplaint:
-			lg.Debugf("%v to %v was marked spam by %v (reported to %v) at %v because of %v", e.TransmissionID, e.Recipient, e.ReportedBy, e.ReportedTo, e.Timestamp)
-		case *Click:
-			lg.Debugf("%v to %v - clicked using %v at %v", e.Recipient, e.TransmissionID, e.GeoIP, e.UserAgent, e.Timestamp)
-		case *Open:
-			lg.Debugf("%v to %v - opened using %v at %v", e.Recipient, e.TransmissionID, e.GeoIP, e.UserAgent, e.Timestamp)
-		case *ListUnsubscribe:
-			lg.Debugf("%v to %v - list unsubscribed at %v", e.TransmissionID, e.Recipient, e.Timestamp)
-		case *LinkUnsubscribe:
-			lg.Debugf("%v to %v - link unsubscribed at %v", e.TransmissionID, e.Recipient, e.Timestamp)
-		case *Unknown:
-			lg.Debugf("wooohoo %v", e)
-		default:
-			lg.Errorf("Can't parse SparkPost event of type: %T: not implemented", e)
+		if unknown, ok := event.(*Unknown); ok {
+			t.Fatal(unknown)
 		}
 	}
 }
