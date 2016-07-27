6d52512da523624cb8d99c7d218a375dce6f7130
Dave Gray <dgray@sparkpost.com>
2016-07-27 09:53:43 -0600

    tests for the other handled cases

--- templates_test.go
+++ templates_test.go
@@ -9,22 +9,96 @@ import (
 )
 
 func TestTemplateValidation(t *testing.T) {
-	from := sp.From{"a@b.com", "A B"}
-	f, err := sp.ParseFrom(from)
+	fromStruct := sp.From{"a@b.com", "A B"}
+	f, err := sp.ParseFrom(fromStruct)
 	if err != nil {
 		t.Error(err)
 		return
 	}
-	if f.Email != from.Email {
+	if fromStruct.Email != f.Email {
 		t.Error(fmt.Errorf("expected email [%s] didn't match actual [%s]",
-			from.Email, f.Email))
+			fromStruct.Email, f.Email))
 		return
 	}
-	if f.Name != from.Name {
+	if fromStruct.Name != f.Name {
 		t.Error(fmt.Errorf("expected name [%s] didn't match actual [%s]",
-			from.Name, f.Name))
+			fromStruct.Name, f.Name))
 		return
 	}
+
+	fromString := "a@b.com"
+	f, err = sp.ParseFrom(fromString)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if fromString != f.Email {
+		t.Error(fmt.Errorf("expected email [%s] didn't match actual [%s]",
+			fromString, f.Email))
+		return
+	}
+	if "" != f.Name {
+		t.Error(fmt.Errorf("expected name to be blank"))
+		return
+	}
+
+	fromMap1 := map[string]interface{}{
+		"name":  "A B",
+		"email": "a@b.com",
+	}
+	f, err = sp.ParseFrom(fromMap1)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	// ParseFrom will bail if these aren't strings
+	fromString, _ = fromMap1["email"].(string)
+	if fromString != f.Email {
+		t.Error(fmt.Errorf("expected email [%s] didn't match actual [%s]",
+			fromString, f.Email))
+		return
+	}
+	nameString, _ := fromMap1["name"].(string)
+	if nameString != f.Name {
+		t.Error(fmt.Errorf("expected name [%s] didn't match actual [%s]",
+			nameString, f.Name))
+		return
+	}
+
+	fromMap1["name"] = 1
+	f, err = sp.ParseFrom(fromMap1)
+	if err == nil {
+		t.Error(fmt.Errorf("failed to detect non-string name"))
+		return
+	}
+
+	fromMap2 := map[string]string{
+		"name":  "A B",
+		"email": "a@b.com",
+	}
+	f, err = sp.ParseFrom(fromMap2)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if fromMap2["email"] != f.Email {
+		t.Error(fmt.Errorf("expected email [%s] didn't match actual [%s]",
+			fromMap2["email"], f.Email))
+		return
+	}
+	if fromMap2["name"] != f.Name {
+		t.Error(fmt.Errorf("expected name [%s] didn't match actual [%s]",
+			fromMap2["name"], f.Name))
+		return
+	}
+
+	fromBytes := []byte("a@b.com")
+	f, err = sp.ParseFrom(fromBytes)
+	if err == nil {
+		t.Error(fmt.Errorf("failed to detect unsupported type"))
+		return
+	}
+
 }
 
 func TestTemplates(t *testing.T) {
