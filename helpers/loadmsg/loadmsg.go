6de9e60e02eec1d610594d18cb275012525be69c
Dave Gray <dgray@sparkpost.com>
2016-07-28 15:52:12 -0600

    pull out dkim headers; wrap errors

--- helpers/loadmsg/loadmsg.go
+++ helpers/loadmsg/loadmsg.go
@@ -2,13 +2,13 @@ package loadmsg
 
 import (
 	"encoding/base64"
-	"fmt"
 	"net/mail"
 	"os"
 	"strconv"
 	"strings"
 
 	"github.com/buger/jsonparser"
+	"github.com/pkg/errors"
 )
 
 type Message struct {
@@ -16,6 +16,7 @@ type Message struct {
 	File       *os.File
 	Message    *mail.Message
 	MSFBL      string
+	DKIM       []string
 	Json       []byte
 	CustID     int
 	Recipient  []byte
@@ -27,22 +28,31 @@ func (m *Message) Load() error {
 
 	m.File, err = os.Open(m.Filename)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "opening file")
 	}
 
 	m.Message, err = mail.ReadMessage(m.File)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "parsing message")
 	}
 
 	if m.ReturnPath == nil {
 		err = m.SetReturnPath(m.Message.Header.Get("Return-Path"))
 		if err != nil {
-			return err
+			return errors.Wrap(err, "setting return path")
 		}
 	}
 
+	for _, hdr := range m.Message.Header["Dkim-Signature"] {
+		m.DKIM = append(m.DKIM, strings.Replace(hdr, " ", "", -1))
+	}
+
 	m.MSFBL = strings.Replace(m.Message.Header.Get("X-MSFBL"), " ", "", -1)
+
+	if m.MSFBL == "" {
+		// early return if there isn't a MSFBL header
+		return nil
+	}
 
 	if strings.Index(m.MSFBL, "|") >= 0 {
 		// Everything before the pipe is an encoded HMAC
@@ -52,22 +62,22 @@ func (m *Message) Load() error {
 
 	m.Json, err = base64.StdEncoding.DecodeString(m.MSFBL)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "decoding fbl")
 	}
 
 	var cid []byte
 	cid, _, _, err = jsonparser.Get(m.Json, "customer_id")
 	if err != nil {
-		return err
+		return errors.Wrap(err, "getting customer_id")
 	}
 	m.CustID, err = strconv.Atoi(string(cid))
 	if err != nil {
-		return err
+		return errors.Wrap(err, "int-ifying customer_id")
 	}
 
 	m.Recipient, _, _, err = jsonparser.Get(m.Json, "r")
 	if err != nil {
-		return err
+		return errors.Wrap(err, "getting recipient")
 	}
 
 	return nil
@@ -75,11 +85,11 @@ func (m *Message) Load() error {
 
 func (m *Message) SetReturnPath(addr string) (err error) {
 	if !strings.Contains(addr, "@") {
-		return fmt.Errorf("Unsupported Return-Path header: no @")
+		return errors.Errorf("Unsupported Return-Path header: no @")
 	}
 	m.ReturnPath, err = mail.ParseAddress(addr)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "parsing return path")
 	}
 	return nil
 }
