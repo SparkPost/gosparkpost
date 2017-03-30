c95e80165793669a920fe9767e731606b14a94a6
Dave Gray <dgray@sparkpost.com>
2017-03-30 15:34:42 -0600

    convert `Content.From` validation to a table test; start on `Template` validation

--- templates_test.go
+++ templates_test.go
@@ -5,119 +5,68 @@ import (
 	"testing"
 
 	sp "github.com/SparkPost/gosparkpost"
+	"github.com/pkg/errors"
 )
 
+var templateFromValidationTests = []struct {
+	in  interface{}
+	err error
+	out sp.From
+}{
+	{sp.From{"a@b.com", "A B"}, nil, sp.From{"a@b.com", "A B"}},
+	{sp.Address{"a@b.com", "A B", "c@d.com"}, nil, sp.From{"a@b.com", "A B"}},
+	{"a@b.com", nil, sp.From{"a@b.com", ""}},
+	{[]byte("a@b.com"), errors.New("unsupported Content.From value type [[]uint8]"), sp.From{"", ""}},
+	{"", errors.New("Content.From may not be empty"), sp.From{"", ""}},
+	{map[string]interface{}{"name": "A B", "email": "a@b.com"}, nil, sp.From{"a@b.com", "A B"}},
+	{map[string]interface{}{"name": 1, "email": "a@b.com"}, errors.New("strings are required for all Content.From values"),
+		sp.From{"a@b.com", ""}},
+	{map[string]string{"name": "A B", "email": "a@b.com"}, nil, sp.From{"a@b.com", "A B"}},
+}
+
+func TestTemplateFromValidation(t *testing.T) {
+	for idx, test := range templateFromValidationTests {
+		f, err := sp.ParseFrom(test.in)
+		if err == nil && test.err != nil || err != nil && test.err == nil {
+			t.Errorf("ParseFrom[%d] => err %q, want %q", idx, err, test.err)
+		} else if err != nil && err.Error() != test.err.Error() {
+			t.Errorf("ParseFrom[%d] => err %q, want %q", idx, err, test.err)
+		} else if f.Email != test.out.Email {
+			t.Errorf("ParseFrom[%d] => Email %q, want %q", idx, f.Email, test.out.Email)
+		} else if f.Name != test.out.Name {
+			t.Errorf("ParseFrom[%d] => Name %q, want %q", idx, f.Name, test.out.Name)
+		}
+	}
+}
+
+var templateValidationTests = []struct {
+	te  *sp.Template
+	err error
+	cmp func(te *sp.Template) bool
+}{
+	{nil, errors.New("Can't Validate a nil Template"), nil},
+	{&sp.Template{}, errors.New("Template requires a non-empty Content.Subject"), nil},
+	{&sp.Template{Content: sp.Content{Subject: "s"}}, errors.New("Template requires either Content.HTML or Content.Text"), nil},
+	{&sp.Template{Content: sp.Content{Subject: "s", HTML: "h", From: ""}},
+		errors.New("Content.From may not be empty"), nil,
+	},
+
+	{
+		&sp.Template{Content: sp.Content{EmailRFC822: "From:foo@example.com\r\n", Subject: "removeme"}},
+		nil,
+		func(te *sp.Template) bool { return te.Content.Subject == "" },
+	},
+}
+
 func TestTemplateValidation(t *testing.T) {
-	fromStruct := sp.From{"a@b.com", "A B"}
-	f, err := sp.ParseFrom(fromStruct)
-	if err != nil {
-		t.Error(err)
-		return
+	for idx, test := range templateValidationTests {
+		err := test.te.Validate()
+		if err == nil && test.err != nil || err != nil && test.err == nil {
+			t.Errorf("Template.Validate[%d] => err %q, want %q", idx, err, test.err)
+		} else if err != nil && err.Error() != test.err.Error() {
+			t.Errorf("Template.Validate[%d] => err %q, want %q", idx, err, test.err)
+		} else if test.cmp != nil && test.cmp(test.te) == false {
+			t.Errorf("Template.Validate[%d] => failed post-condition check for %q", test.te)
+		}
 	}
-	if fromStruct.Email != f.Email {
-		t.Error(fmt.Errorf("expected email [%s] didn't match actual [%s]",
-			fromStruct.Email, f.Email))
-		return
-	}
-	if fromStruct.Name != f.Name {
-		t.Error(fmt.Errorf("expected name [%s] didn't match actual [%s]",
-			fromStruct.Name, f.Name))
-		return
-	}
-
-	addrStruct := sp.Address{"a@b.com", "A B", "c@d.com"}
-	f, err = sp.ParseFrom(addrStruct)
-	if err != nil {
-		t.Error(err)
-		return
-	}
-	if addrStruct.Email != f.Email {
-		t.Error(fmt.Errorf("expected email [%s] didn't match actual [%s]",
-			addrStruct.Email, f.Email))
-		return
-	}
-	if addrStruct.Name != f.Name {
-		t.Error(fmt.Errorf("expected name [%s] didn't match actual [%s]",
-			addrStruct.Name, f.Name))
-		return
-	}
-
-	fromString := "a@b.com"
-	f, err = sp.ParseFrom(fromString)
-	if err != nil {
-		t.Error(err)
-		return
-	}
-	if fromString != f.Email {
-		t.Error(fmt.Errorf("expected email [%s] didn't match actual [%s]",
-			fromString, f.Email))
-		return
-	}
-	if "" != f.Name {
-		t.Error(fmt.Errorf("expected name to be blank"))
-		return
-	}
-	fromString = ""
-	_, err = sp.ParseFrom(fromString)
-	if err == nil {
-		t.Error(fmt.Errorf("Content.From should not be allowed!"))
-	}
-
-	fromMap1 := map[string]interface{}{
-		"name":  "A B",
-		"email": "a@b.com",
-	}
-	f, err = sp.ParseFrom(fromMap1)
-	if err != nil {
-		t.Error(err)
-		return
-	}
-	// ParseFrom will bail if these aren't strings
-	fromString, _ = fromMap1["email"].(string)
-	if fromString != f.Email {
-		t.Error(fmt.Errorf("expected email [%s] didn't match actual [%s]",
-			fromString, f.Email))
-		return
-	}
-	nameString, _ := fromMap1["name"].(string)
-	if nameString != f.Name {
-		t.Error(fmt.Errorf("expected name [%s] didn't match actual [%s]",
-			nameString, f.Name))
-		return
-	}
-
-	fromMap1["name"] = 1
-	f, err = sp.ParseFrom(fromMap1)
-	if err == nil {
-		t.Error(fmt.Errorf("failed to detect non-string name"))
-		return
-	}
-
-	fromMap2 := map[string]string{
-		"name":  "A B",
-		"email": "a@b.com",
-	}
-	f, err = sp.ParseFrom(fromMap2)
-	if err != nil {
-		t.Error(err)
-		return
-	}
-	if fromMap2["email"] != f.Email {
-		t.Error(fmt.Errorf("expected email [%s] didn't match actual [%s]",
-			fromMap2["email"], f.Email))
-		return
-	}
-	if fromMap2["name"] != f.Name {
-		t.Error(fmt.Errorf("expected name [%s] didn't match actual [%s]",
-			fromMap2["name"], f.Name))
-		return
-	}
-
-	fromBytes := []byte("a@b.com")
-	f, err = sp.ParseFrom(fromBytes)
-	if err == nil {
-		t.Error(fmt.Errorf("failed to detect unsupported type"))
-		return
-	}
-
 }
