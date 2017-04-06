2be1daae8114d4cdd12c2e0703c33dc8e53a5e7d
Dave Gray <dgray@sparkpost.com>
2017-04-06 12:44:01 -0600

    first `Client.Templates` test; move test table definitions inside test functions

--- templates_test.go
+++ templates_test.go
@@ -12,25 +12,23 @@ import (
 	"github.com/pkg/errors"
 )
 
-var templateFromValidationTests = []struct {
-	in  interface{}
-	err error
-	out sp.From
-}{
-	{sp.From{"a@b.com", "A B"}, nil, sp.From{"a@b.com", "A B"}},
-	{sp.Address{"a@b.com", "A B", "c@d.com"}, nil, sp.From{"a@b.com", "A B"}},
-	{"a@b.com", nil, sp.From{"a@b.com", ""}},
-	{nil, errors.New("unsupported Content.From value type [%!s(<nil>)]"), sp.From{"", ""}},
-	{[]byte("a@b.com"), errors.New("unsupported Content.From value type [[]uint8]"), sp.From{"", ""}},
-	{"", errors.New("Content.From may not be empty"), sp.From{"", ""}},
-	{map[string]interface{}{"name": "A B", "email": "a@b.com"}, nil, sp.From{"a@b.com", "A B"}},
-	{map[string]interface{}{"name": 1, "email": "a@b.com"}, errors.New("strings are required for all Content.From values"),
-		sp.From{"a@b.com", ""}},
-	{map[string]string{"name": "A B", "email": "a@b.com"}, nil, sp.From{"a@b.com", "A B"}},
-}
-
 func TestTemplateFromValidation(t *testing.T) {
-	for idx, test := range templateFromValidationTests {
+	for idx, test := range []struct {
+		in  interface{}
+		err error
+		out sp.From
+	}{
+		{sp.From{"a@b.com", "A B"}, nil, sp.From{"a@b.com", "A B"}},
+		{sp.Address{"a@b.com", "A B", "c@d.com"}, nil, sp.From{"a@b.com", "A B"}},
+		{"a@b.com", nil, sp.From{"a@b.com", ""}},
+		{nil, errors.New("unsupported Content.From value type [%!s(<nil>)]"), sp.From{"", ""}},
+		{[]byte("a@b.com"), errors.New("unsupported Content.From value type [[]uint8]"), sp.From{"", ""}},
+		{"", errors.New("Content.From may not be empty"), sp.From{"", ""}},
+		{map[string]interface{}{"name": "A B", "email": "a@b.com"}, nil, sp.From{"a@b.com", "A B"}},
+		{map[string]interface{}{"name": 1, "email": "a@b.com"}, errors.New("strings are required for all Content.From values"),
+			sp.From{"a@b.com", ""}},
+		{map[string]string{"name": "A B", "email": "a@b.com"}, nil, sp.From{"a@b.com", "A B"}},
+	} {
 		f, err := sp.ParseFrom(test.in)
 		if err == nil && test.err != nil || err != nil && test.err == nil {
 			t.Errorf("ParseFrom[%d] => err %q, want %q", idx, err, test.err)
@@ -75,59 +73,57 @@ func TestTemplateOptions(t *testing.T) {
 	}
 }
 
-var templateValidationTests = []struct {
-	te  *sp.Template
-	err error
-	cmp func(te *sp.Template) bool
-}{
-	{nil, errors.New("Can't Validate a nil Template"), nil},
-	{&sp.Template{}, errors.New("Template requires a non-empty Content.Subject"), nil},
-	{&sp.Template{Content: sp.Content{Subject: "s"}}, errors.New("Template requires either Content.HTML or Content.Text"), nil},
-	{&sp.Template{Content: sp.Content{Subject: "s", HTML: "h", From: ""}},
-		errors.New("Content.From may not be empty"), nil},
-
-	{&sp.Template{ID: strings.Repeat("id", 33), Content: sp.Content{Subject: "s", HTML: "h", From: "f"}},
-		errors.New("Template id may not be longer than 64 bytes"), nil},
-	{&sp.Template{Name: strings.Repeat("name", 257), Content: sp.Content{Subject: "s", HTML: "h", From: "f"}},
-		errors.New("Template name may not be longer than 1024 bytes"), nil},
-	{&sp.Template{Description: strings.Repeat("desc", 257), Content: sp.Content{Subject: "s", HTML: "h", From: "f"}},
-		errors.New("Template description may not be longer than 1024 bytes"), nil},
-
-	{&sp.Template{
-		Content: sp.Content{
-			Subject: "s", HTML: "h", From: "f",
-			Attachments: []sp.Attachment{{Filename: strings.Repeat("f", 256)}},
-		}},
-		errors.Errorf("Attachment name length must be <= 255: [%s]", strings.Repeat("f", 256)), nil},
-	{&sp.Template{
-		Content: sp.Content{
-			Subject: "s", HTML: "h", From: "f",
-			Attachments: []sp.Attachment{{B64Data: "\r\n"}},
-		}},
-		errors.New("Attachment data may not contain line breaks [\\r\\n]"), nil},
-
-	{&sp.Template{
-		Content: sp.Content{
-			Subject: "s", HTML: "h", From: "f",
-			InlineImages: []sp.InlineImage{{Filename: strings.Repeat("f", 256)}},
-		}},
-		errors.Errorf("InlineImage name length must be <= 255: [%s]", strings.Repeat("f", 256)), nil},
-	{&sp.Template{
-		Content: sp.Content{
-			Subject: "s", HTML: "h", From: "f",
-			InlineImages: []sp.InlineImage{{B64Data: "\r\n"}},
-		}},
-		errors.New("InlineImage data may not contain line breaks [\\r\\n]"), nil},
-
-	{
-		&sp.Template{Content: sp.Content{EmailRFC822: "From:foo@example.com\r\n", Subject: "removeme"}},
-		nil,
-		func(te *sp.Template) bool { return te.Content.Subject == "" },
-	},
-}
-
 func TestTemplateValidation(t *testing.T) {
-	for idx, test := range templateValidationTests {
+	for idx, test := range []struct {
+		te  *sp.Template
+		err error
+		cmp func(te *sp.Template) bool
+	}{
+		{nil, errors.New("Can't Validate a nil Template"), nil},
+		{&sp.Template{}, errors.New("Template requires a non-empty Content.Subject"), nil},
+		{&sp.Template{Content: sp.Content{Subject: "s"}}, errors.New("Template requires either Content.HTML or Content.Text"), nil},
+		{&sp.Template{Content: sp.Content{Subject: "s", HTML: "h", From: ""}},
+			errors.New("Content.From may not be empty"), nil},
+
+		{&sp.Template{ID: strings.Repeat("id", 33), Content: sp.Content{Subject: "s", HTML: "h", From: "f"}},
+			errors.New("Template id may not be longer than 64 bytes"), nil},
+		{&sp.Template{Name: strings.Repeat("name", 257), Content: sp.Content{Subject: "s", HTML: "h", From: "f"}},
+			errors.New("Template name may not be longer than 1024 bytes"), nil},
+		{&sp.Template{Description: strings.Repeat("desc", 257), Content: sp.Content{Subject: "s", HTML: "h", From: "f"}},
+			errors.New("Template description may not be longer than 1024 bytes"), nil},
+
+		{&sp.Template{
+			Content: sp.Content{
+				Subject: "s", HTML: "h", From: "f",
+				Attachments: []sp.Attachment{{Filename: strings.Repeat("f", 256)}},
+			}},
+			errors.Errorf("Attachment name length must be <= 255: [%s]", strings.Repeat("f", 256)), nil},
+		{&sp.Template{
+			Content: sp.Content{
+				Subject: "s", HTML: "h", From: "f",
+				Attachments: []sp.Attachment{{B64Data: "\r\n"}},
+			}},
+			errors.New("Attachment data may not contain line breaks [\\r\\n]"), nil},
+
+		{&sp.Template{
+			Content: sp.Content{
+				Subject: "s", HTML: "h", From: "f",
+				InlineImages: []sp.InlineImage{{Filename: strings.Repeat("f", 256)}},
+			}},
+			errors.Errorf("InlineImage name length must be <= 255: [%s]", strings.Repeat("f", 256)), nil},
+		{&sp.Template{
+			Content: sp.Content{
+				Subject: "s", HTML: "h", From: "f",
+				InlineImages: []sp.InlineImage{{B64Data: "\r\n"}},
+			}},
+			errors.New("InlineImage data may not contain line breaks [\\r\\n]"), nil},
+
+		{
+			&sp.Template{Content: sp.Content{EmailRFC822: "From:foo@example.com\r\n", Subject: "removeme"}},
+			nil,
+			func(te *sp.Template) bool { return te.Content.Subject == "" },
+		},
+	} {
 		err := test.te.Validate()
 		if err == nil && test.err != nil || err != nil && test.err == nil {
 			t.Errorf("Template.Validate[%d] => err %q, want %q", idx, err, test.err)
@@ -139,40 +135,38 @@ func TestTemplateValidation(t *testing.T) {
 	}
 }
 
-var templateCreateTests = []struct {
-	in     *sp.Template
-	err    error
-	status int
-	json   string
-	id     string
-}{
-	{nil, errors.New("Create called with nil Template"), 0, "", ""},
-	{&sp.Template{}, errors.New("Template requires a non-empty Content.Subject"), 0, "", ""},
-	{&sp.Template{Content: sp.Content{Subject: "s", HTML: "h", From: "f"}},
-		errors.New("Unexpected response to Template creation (results)"),
-		200, `{"foo":{"id":"new-template"}}`, ""},
-	{&sp.Template{Content: sp.Content{Subject: "s", HTML: "h", From: "f"}},
-		errors.New("Unexpected response to Template creation"),
-		200, `{"results":{"ID":"new-template"}}`, ""},
-	{&sp.Template{Content: sp.Content{Subject: "s", HTML: "h", From: "f"}},
-		errors.New("parsing api response: unexpected end of JSON input"),
-		200, `{"results":{"ID":"new-template"}`, ""},
-
-	{&sp.Template{Content: sp.Content{Subject: "s{{", HTML: "h", From: "f"}},
-		sp.SPErrors([]sp.SPError{{
-			Message:     "substitution language syntax error in template content",
-			Description: "Error while compiling header Subject: substitution statement missing ending '}}'",
-			Code:        "3000",
-			Part:        "Header:Subject",
-		}}),
-		422, `{ "errors": [ { "message": "substitution language syntax error in template content", "description": "Error while compiling header Subject: substitution statement missing ending '}}'", "code": "3000", "part": "Header:Subject" } ] }`, ""},
-
-	{&sp.Template{Content: sp.Content{Subject: "s", HTML: "h", From: "f"}}, nil,
-		200, `{"results":{"id":"new-template"}}`, "new-template"},
-}
-
 func TestTemplateCreate(t *testing.T) {
-	for idx, test := range templateCreateTests {
+	for idx, test := range []struct {
+		in     *sp.Template
+		err    error
+		status int
+		json   string
+		id     string
+	}{
+		{nil, errors.New("Create called with nil Template"), 0, "", ""},
+		{&sp.Template{}, errors.New("Template requires a non-empty Content.Subject"), 0, "", ""},
+		{&sp.Template{Content: sp.Content{Subject: "s", HTML: "h", From: "f"}},
+			errors.New("Unexpected response to Template creation (results)"),
+			200, `{"foo":{"id":"new-template"}}`, ""},
+		{&sp.Template{Content: sp.Content{Subject: "s", HTML: "h", From: "f"}},
+			errors.New("Unexpected response to Template creation"),
+			200, `{"results":{"ID":"new-template"}}`, ""},
+		{&sp.Template{Content: sp.Content{Subject: "s", HTML: "h", From: "f"}},
+			errors.New("parsing api response: unexpected end of JSON input"),
+			200, `{"results":{"ID":"new-template"}`, ""},
+
+		{&sp.Template{Content: sp.Content{Subject: "s{{", HTML: "h", From: "f"}},
+			sp.SPErrors([]sp.SPError{{
+				Message:     "substitution language syntax error in template content",
+				Description: "Error while compiling header Subject: substitution statement missing ending '}}'",
+				Code:        "3000",
+				Part:        "Header:Subject",
+			}}),
+			422, `{ "errors": [ { "message": "substitution language syntax error in template content", "description": "Error while compiling header Subject: substitution statement missing ending '}}'", "code": "3000", "part": "Header:Subject" } ] }`, ""},
+
+		{&sp.Template{Content: sp.Content{Subject: "s", HTML: "h", From: "f"}}, nil,
+			200, `{"results":{"id":"new-template"}}`, "new-template"},
+	} {
 		testSetup(t)
 		defer testTeardown()
 
@@ -195,31 +189,29 @@ func TestTemplateCreate(t *testing.T) {
 	}
 }
 
-var templateUpdateTests = []struct {
-	in     *sp.Template
-	err    error
-	status int
-	json   string
-}{
-	{nil, errors.New("Update called with nil Template"), 0, ""},
-	{&sp.Template{ID: ""}, errors.New("Update called with blank id"), 0, ""},
-	{&sp.Template{ID: "id", Content: sp.Content{}}, errors.New("Template requires a non-empty Content.Subject"), 0, ""},
-	{&sp.Template{ID: "id", Content: sp.Content{Subject: "s", HTML: "h", From: "f"}}, errors.New("parsing api response: unexpected end of JSON input"), 0, `{ "errors": [ { "message": "truncated json" }`},
-
-	{&sp.Template{ID: "id", Content: sp.Content{Subject: "s{{", HTML: "h", From: "f"}},
-		sp.SPErrors([]sp.SPError{{
-			Message:     "substitution language syntax error in template content",
-			Description: "Error while compiling header Subject: substitution statement missing ending '}}'",
-			Code:        "3000",
-			Part:        "Header:Subject",
-		}}),
-		422, `{ "errors": [ { "message": "substitution language syntax error in template content", "description": "Error while compiling header Subject: substitution statement missing ending '}}'", "code": "3000", "part": "Header:Subject" } ] }`},
-
-	{&sp.Template{ID: "id", Content: sp.Content{Subject: "s", HTML: "h", From: "f"}}, nil, 200, ""},
-}
-
 func TestTemplateUpdate(t *testing.T) {
-	for idx, test := range templateUpdateTests {
+	for idx, test := range []struct {
+		in     *sp.Template
+		err    error
+		status int
+		json   string
+	}{
+		{nil, errors.New("Update called with nil Template"), 0, ""},
+		{&sp.Template{ID: ""}, errors.New("Update called with blank id"), 0, ""},
+		{&sp.Template{ID: "id", Content: sp.Content{}}, errors.New("Template requires a non-empty Content.Subject"), 0, ""},
+		{&sp.Template{ID: "id", Content: sp.Content{Subject: "s", HTML: "h", From: "f"}}, errors.New("parsing api response: unexpected end of JSON input"), 0, `{ "errors": [ { "message": "truncated json" }`},
+
+		{&sp.Template{ID: "id", Content: sp.Content{Subject: "s{{", HTML: "h", From: "f"}},
+			sp.SPErrors([]sp.SPError{{
+				Message:     "substitution language syntax error in template content",
+				Description: "Error while compiling header Subject: substitution statement missing ending '}}'",
+				Code:        "3000",
+				Part:        "Header:Subject",
+			}}),
+			422, `{ "errors": [ { "message": "substitution language syntax error in template content", "description": "Error while compiling header Subject: substitution statement missing ending '}}'", "code": "3000", "part": "Header:Subject" } ] }`},
+
+		{&sp.Template{ID: "id", Content: sp.Content{Subject: "s", HTML: "h", From: "f"}}, nil, 200, ""},
+	} {
 		testSetup(t)
 		defer testTeardown()
 
@@ -245,16 +237,30 @@ func TestTemplateUpdate(t *testing.T) {
 	}
 }
 
-var templatesTests = []struct {
-	err    error
-	status int
-	json   string
-}{}
-
 func TestTemplates(t *testing.T) {
-	for idx, test := range templatesTests {
+	for idx, test := range []struct {
+		err    error
+		status int
+		json   string
+	}{
+		{errors.New("parsing api response: unexpected end of JSON input"), 0, `{ "errors": [ { "message": "truncated json" }`},
+	} {
 		testSetup(t)
 		defer testTeardown()
 
+		path := fmt.Sprintf(sp.TemplatesPathFormat, testClient.Config.ApiVersion)
+		testMux.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+			testMethod(t, r, "GET")
+			w.Header().Set("Content-Type", "application/json; charset=utf8")
+			w.WriteHeader(test.status)
+			w.Write([]byte(test.json))
+		})
+
+		_, _, err := testClient.Templates()
+		if err == nil && test.err != nil || err != nil && test.err == nil {
+			t.Errorf("Templates[%d] => err %q want %q", idx, err, test.err)
+		} else if err != nil && err.Error() != test.err.Error() {
+			t.Errorf("Templates[%d] => err %q want %q", idx, err, test.err)
+		}
 	}
 }
