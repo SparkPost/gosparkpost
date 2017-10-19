06da495da2325a4bc09435aafdfe7ebefde169b0
Dave Gray <dgray@sparkpost.com>
2017-10-19 10:01:33 -0600

    attempt to reproduce reported marshalling error

--- events/events_test.go
+++ events/events_test.go
@@ -1,11 +1,46 @@
 package events
 
 import (
+	"bytes"
 	"encoding/json"
 	"io/ioutil"
 	"os"
+	"reflect"
 	"testing"
 )
+
+func TestGeoIPMarshalling(t *testing.T) {
+	for idx, test := range []struct {
+		in   GeoIP
+		err  error
+		json []byte
+	}{
+		{
+			GeoIP{"USA", "CO", "Denver", 39.7392, 104.9903}, nil,
+			[]byte(`{"country":"USA","region":"CO","city":"Denver","latitude":39.7392,"longitude":104.9903}`),
+		},
+	} {
+		jsonBytes, err := json.Marshal(test.in)
+		if err != nil {
+			if test.err != nil && test.err == err {
+				// ignore expected errors
+			} else {
+				t.Fatal(err)
+			}
+		}
+		if bytes.Compare(jsonBytes, test.json) != 0 {
+			t.Errorf("Marshal[%d] => got/want:\n%s\n%s", idx, string(jsonBytes), string(test.json))
+		}
+		geo := GeoIP{}
+		err = json.Unmarshal(jsonBytes, &geo)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !reflect.DeepEqual(test.in, geo) {
+			t.Errorf("Unmarshal[%d] => got/want:\n%q\n%q", idx, geo, test.in)
+		}
+	}
+}
 
 func TestSampleEvents(t *testing.T) {
 	file, err := os.Open("sample-events.json")
